9989b1f7458f4df93a4531f24ed8514dc8ae9017
a1l1ke <onetenthplease@gmail.com>
2026-07-06 11:09:20 +0900

    chore: Java 17 컴파일러 설정 추가 및 Main 주석 보강
    - pom.xml에 자바 컴파일러 소스 및 타겟 버전을 17로 명시하여 자바 17 환경 구축
    - pom.xml 들여쓰기 등 코드 정렬 포맷팅 수정
    - Main.java에 record 객체와 일반 객체의 참조 차이점에 대한 주석 추가

--- src/main/java/org/example/step1/Main.java
+++ src/main/java/org/example/step1/Main.java
@@ -21,5 +21,7 @@ public class Main {
         System.out.println("book = " + book);
         System.out.println("registeredBook = " + registeredBook);
         System.out.println("book == registeredBook : %b".formatted(book == registeredBook));
+        // 일반적인 객체면 registeredBook == book <- 저장한 객체와 꺼내는 객체가 같을 경우
+        // 참조 이슈가 일어나서 수정 시 저장된 것도 수정하는 문제가 생기는데, record 경우에는 그럴 이슈 X.
     }
 }
