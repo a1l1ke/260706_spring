13249ba4ebebaccf189162cf3de37d82b6325e0a
a1l1ke <onetenthplease@gmail.com>
2026-07-06 11:05:00 +0900

    feat: BookServiceImpl 도서 등록/조회 기능 구현 및 Main 실행 예제 추가
    - BookServiceImpl에 MemoryBookRepository 의존성을 추가하여 register(저장) 및 findBook(조회) 비즈니스 로직 완성
    - Main 클래스에서 BookService 구현체와 도서 객체를 생성해 도서 등록 및 조회 동작 검증 예제 작성
    - 조회된 도서의 정보와 원본 객체 간의 동일성(==) 여부를 검증하기 위한 콘솔 출력문 추가

--- src/main/java/org/example/step1/Main.java
+++ src/main/java/org/example/step1/Main.java
@@ -1,14 +1,25 @@
 package org.example.step1;
 
 
+import org.example.step1.application.BookServiceImpl;
+import org.example.step1.domain.Book;
+import org.example.step1.domain.BookService;
+
 public class Main {
     // 진입점 -> entrypoint (실행 기준점 static main)
     public static void main(String[] args) {
         // BookService -> Book. -> 추상화.
 //        BookService bookService;
+        BookService bookService = new BookServiceImpl();
 //        Book book = new Book(...);
+        Book book = new Book(1, "윌리엄의 코딩비밀");
 //        bookService.register(book);
+        bookService.register(book);
         // bookService.findBook(...);
+        Book registeredBook = bookService.findBook(1);
         // ...
+        System.out.println("book = " + book);
+        System.out.println("registeredBook = " + registeredBook);
+        System.out.println("book == registeredBook : %b".formatted(book == registeredBook));
     }
 }
--- src/main/java/org/example/step1/application/BookServiceImpl.java
+++ src/main/java/org/example/step1/application/BookServiceImpl.java
@@ -1,19 +1,22 @@
 package org.example.step1.application;
 
 import org.example.step1.domain.Book;
+import org.example.step1.domain.BookRepository;
 import org.example.step1.domain.BookService;
+import org.example.step1.infra.MemoryBookRepository;
 
 // 연관된 인프라나 api 등이 명확하지 않아서 Impl(구현체)라는 것을 클래스 이름에 명시
 // 인터페이스를 써서 추상화를 하고 싶긴한데 다형성이 꼭 필요하진 않은 것
 public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository = new MemoryBookRepository();
 
     @Override
     public void register(Book book) {
-
+        bookRepository.save(book);
     }
 
     @Override
     public Book findBook(long bookId) {
-        return null;
+        return bookRepository.findBook(bookId);
     }
 }
